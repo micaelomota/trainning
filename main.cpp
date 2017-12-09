91a0928260a0f74c47bd5adc1bd55e0ff77dfdf4
Micael Mota <micaelomota@gmail.com>
2017-12-09 13:49:51 -0300

    first commit - Learning LCA

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	int t;
+
+	cin >> t;
+
+	while (t--) {
+		int n, m;
+
+		cin >> n;
+
+		for (int i=0; i<n; ++i) {
+			cin >> m;
+
+			
+		}
+	}
+
+
+	return 0;
+}
